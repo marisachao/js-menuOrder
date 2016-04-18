d42c684662d703e979f6b61221fdb2ecebc5bbe1
marisachao <marisa.chao@wustl.edu>
2016-04-18 18:27:09 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -4,9 +4,14 @@ var order = '';
 
 // build a function to ask questions
 
-function question() {
-  // your code goes here
+function question(questionText) {
+  var answer = prompt(questionText);
+  order = order + answer
 }
+
+question("do you want a hamburger or cheeseburger?")
+question("french fries or animal fries?");
+question("chocolate, strawberry, or vanilla milkshake?");
 
 // call the QUESTION function with the order options
 question();
