d1e8858578d28edce1e301ac1428ed1ed71eafca
marisachao <marisa.chao@wustl.edu>
2016-04-25 18:18:01 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,18 +1,27 @@
 // begin with a blank/new order
-var order = '';
+//var order = '';
 
 
 // build a function to ask questions
 
-function question(questionText) {
-  var answer = prompt(questionText);
-  order = order + " " + answer
+//function question(questionText) {
+  //var answer = prompt(questionText);
+  //order = order + " " + answer
+//}
+
+var myQuestion = [
+  "do you want a hamburger or cheeseburger?", 
+  "french fries or animal fries?", 
+  "chocolate, strawberry, or vanilla milkshake?"
+  ];
+
+var i =0;
+var order = '';
+while (i<myQuestion.length) {
+  var answerInput = prompt(myQuestions[i]);
+  order = order + " " + answerInput;
+  i++;
 }
-
-question("do you want a hamburger or cheeseburger?")
-question("french fries or animal fries?");
-question("chocolate, strawberry, or vanilla milkshake?");
-
 
 // output their order
 document.write("<p>You ordered: " + order + "<p>");
